eac80bccb3479d421d9fa652928a0d857bd75a2e
Alex Dickson <alexrd@msu.edu>
2018-07-13 12:19:17 -0400

    clean up

--- examples/DicksonRingPotential/env.sh
+++
@@ -1,4 +0,0 @@
-export WEST_ROOT=/Users/lev/Documents/Projects/github/westpa
-export WEST_PYTHON=$(which python2.7)
-export WM_WORK_MANAGER=serial
-
--- examples/DicksonRingPotential/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source env.sh
-
-rm -f west.h5
-BSTATES="--bstate initA,1.0"
-$WEST_ROOT/bin/w_init $BSTATES "$@"
-
